da2b1aa56bc7ae24e8e06f00544244c13d4e1f22
George <me@georgebutter.com>
2017-11-26 06:33:00 +1100

    no twilio

--- controllers/api.js
+++ controllers/api.js
@@ -363,42 +363,42 @@ exports.postStripe = (req, res) => {
   });
 };
 
-/**
- * GET /api/twilio
- * Twilio API example.
- */
-exports.getTwilio = (req, res) => {
-  res.render('api/twilio', {
-    title: 'Twilio API'
-  });
-};
-
-/**
- * POST /api/twilio
- * Send a text message using Twilio.
- */
-exports.postTwilio = (req, res, next) => {
-  req.assert('number', 'Phone number is required.').notEmpty();
-  req.assert('message', 'Message cannot be blank.').notEmpty();
-
-  const errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/api/twilio');
-  }
-
-  const message = {
-    to: req.body.number,
-    from: '+13472235148',
-    body: req.body.message
-  };
-  twilio.sendMessage(message, (err, responseData) => {
-    if (err) { return next(err.message); }
-    req.flash('success', { msg: `Text sent to ${responseData.to}.` });
-    res.redirect('/api/twilio');
-  });
-};
+// /**
+//  * GET /api/twilio
+//  * Twilio API example.
+//  */
+// exports.getTwilio = (req, res) => {
+//   res.render('api/twilio', {
+//     title: 'Twilio API'
+//   });
+// };
+//
+// /**
+//  * POST /api/twilio
+//  * Send a text message using Twilio.
+//  */
+// exports.postTwilio = (req, res, next) => {
+//   req.assert('number', 'Phone number is required.').notEmpty();
+//   req.assert('message', 'Message cannot be blank.').notEmpty();
+//
+//   const errors = req.validationErrors();
+//
+//   if (errors) {
+//     req.flash('errors', errors);
+//     return res.redirect('/api/twilio');
+//   }
+//
+//   const message = {
+//     to: req.body.number,
+//     from: '+13472235148',
+//     body: req.body.message
+//   };
+//   twilio.sendMessage(message, (err, responseData) => {
+//     if (err) { return next(err.message); }
+//     req.flash('success', { msg: `Text sent to ${responseData.to}.` });
+//     res.redirect('/api/twilio');
+//   });
+// };
 
 /**
  * GET /api/clockwork
